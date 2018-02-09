67687a7d27cb88824ddd926686233595a179f93e
julio <julio.parra@me.com>
2018-02-08 17:33:56 -0800

    Added top level 3 and 4 loops

---
+++ toplevel_canonical_3.m
@@ -0,0 +1,20 @@
+(* ::Package:: *)
+
+(*My choice of labels*)
+
+
+(* Top level vacuum graphs with ready for canonical labels *)
+top[1] = {{-1, 2, 5}, {-2, 3, 7}, {-3, 1, -6}, {6, -5, -7}};
+
+
+
+(*Choice of labels*)
+
+
+vars = {q[1] = k[1], 
+       q[2] = k[2], 
+       q[3] = k[3], 
+       q[4] = k[4], 
+       q[5] = k[1] - k[2],
+       q[6] = k[1] - k[3],
+       q[7] = k[2] - k[3]};
---
+++ toplevel_canonical_4.m
@@ -0,0 +1,24 @@
+(* ::Package:: *)
+
+(*My choice of labels*)
+
+
+(* Top level vacuum graphs with ready for canonical labels *)
+prism= {{1, -2, -8}, {-1, 3, 9}, {8, 6, -5}, {5, -9, -7}, {-3, 7, 4}, {-4, -6, 2}};
+
+xprism= {{-10, -2, 9}, {10, 3, -8}, {8, 6, -5}, {5, -9, -7}, {-3, 7, 4}, {-4, -6, 2}};
+
+
+(*Choice of labels*)
+
+
+vars = {q[1] = k[1], 
+       q[2] = k[2], 
+       q[3] = k[3], 
+       q[4] = k[4], 
+       q[5] = k[1] - k[4],
+       q[6] = k[2] - k[4],
+       q[7] = k[3] - k[4], 
+       q[8] = k[1] - k[2], 
+       q[9] = k[1] \[Minus] k[3], 
+       q[10] = k[1]- k[2] \[Minus] k[3]};
